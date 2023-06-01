5a5d96b20c52a9754dc2766bb86a42ec62593ab5
kurmicshare <kurmi.cshare@gmail.com>
2023-06-01 23:37:28 +0530

    remove console from server

--- index.js
+++ index.js
@@ -24,32 +24,25 @@ io.on("connection", (socket) => {
   console.log("socket_id", socket.id);
 
   connectedUsers.push(socket.id);
-  console.log(connectedUsers);
   socket.emit("me", socket.id);
   socket.emit("allUser", connectedUsers);
 
   socket.on("test_connection", (message) => {
-    console.log("Received message:", message);
     io.emit("test_connection", message);
   });
 
   socket.on("callEndEvent", (message) => {
-    console.log(message, "message");
     if (message === "end") {
       io.emit("callEndEvent", message); // Broadcast the "end" message to all connected clients
     }
   });
 
   socket.on("disconnect", () => {
-    console.log("disconnect");
     connectedUsers = connectedUsers.filter((id) => id != socket.id);
     socket.broadcast.emit("callEnded");
   });
 
   socket.on("callUser", ({ userToCall, signalData, from, name }) => {
-    console.log("userToCall", userToCall);
-    console.log("from", from);
-    console.log("name", name);
     io.to(userToCall).emit("callUser", { signal: signalData, from, name });
   });
 
